2caa2b9cbcfa7e2849de2922ababf03aa4697683
Minajul <ruleverse.tech@gmail.com>
2026-03-25 15:31:41 +0530

    my first commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+print('hello worl')
+rakuda=int(input('Raku da enter your age'))
+if (rakuda==5):
+    print('yhhh')
+else:
+    print('Raku da ')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+import requests
+res=requests.get("https://api.github.com")
+print(res.status_code)
